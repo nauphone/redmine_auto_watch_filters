62db05e896bcccbe2323103347b26fc46063f056
Alex Chrome <dify.chrome@gmail.com>
2012-04-23 13:47:14 +0700

    Hide settings tab if module is disabled

--- lib/projects_helper_patch.rb
+++ lib/projects_helper_patch.rb
@@ -6,7 +6,14 @@ module AutoWatchGroup
       base.class_eval do
         def project_settings_tabs_with_auto_watch_tabs
           tabs = project_settings_tabs_without_auto_watch_tabs
-          tabs.push({:name => "auto_watch_filters", :action => :manage_auto_watch, :partial => 'auto_watch_filters/index', :label => :label_auto_watch_filter_plural})
+          unless @project.enabled_modules.find_by_name('auto_watch').nil?
+            tabs.push({
+              :name => "auto_watch_filters",
+              :action => :manage_auto_watch,
+              :partial => 'auto_watch_filters/index',
+              :label => :label_auto_watch_filter_plural
+            })
+          end
           tabs
         end
         alias_method_chain :project_settings_tabs, :auto_watch_tabs
