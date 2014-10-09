4d2edac8291e214d46201d503f8888efd4bb1c76
Alexei Margasov <amargasov@naumen.ru>
2014-10-09 18:46:14 +0600

    fix migration

--- db/migrate/20141006145300_add_query_id_to_watch_filters.rb
+++ db/migrate/20141006145300_add_query_id_to_watch_filters.rb
@@ -1,9 +1,28 @@
 class AddQueryIdToWatchFilters < ActiveRecord::Migration
   def self.up
     add_column :auto_watch_filters, :query_id, :integer
+    AutoWatchFilter.all.each do |watch_filter|
+      if watch_filter.query.nil?
+        @project = Project.find(watch_filter.project_id)
+        new_query = AutoWatchQuery.new(:filters => watch_filter.filters, :name => watch_filter.name, :is_public => true, :user_id => 0)
+        new_query.project_id = watch_filter.project_id.to_i
+        new_query.save
+        watch_filter.query_id = new_query.id
+        watch_filter.save
+      end
+    end
+    remove_column :auto_watch_filters, :filters
   end
 
   def self.down
+    add_column :auto_watch_filters, :filters, :text
+    AutoWatchFilter.all.each do |watch_filter|
+      unless watch_filter.query.nil?
+        watch_filter.filters = watch_filter.query.filters
+        watch_filter.save
+        watch_filter.query.destroy
+      end
+    end
     remove_column :auto_watch_filters, :query_id
   end
 end
