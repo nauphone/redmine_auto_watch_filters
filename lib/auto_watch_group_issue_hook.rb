4dc791c595d511e1f9d4aea79d3ab06934e32195
Alexei Margasov <amargasov@naumen.ru>
2015-03-17 17:28:24 +0500

    add notifiaction

--- lib/auto_watch_group_issue_hook.rb
+++ lib/auto_watch_group_issue_hook.rb
@@ -6,6 +6,9 @@ class AutoWatchGroupIssuesControllerHook < Redmine::Hook::ViewListener
 
   def controller_issues_edit_after_save(context={})
     issue = context[:issue]
+    if Redmine::Plugin.installed? :redmine_advanced_issue_history
+      notes=[]
+    end
     unless issue.project.enabled_modules.find_by_name('auto_watch').nil?
       AutoWatchFilter.all.each do |watch_filter|
         next if watch_filter.group.nil?
@@ -15,11 +18,20 @@ class AutoWatchGroupIssuesControllerHook < Redmine::Hook::ViewListener
         unless issue_ids.nil? or user_ids.nil?
           if issue_ids.include? issue.id
             user_ids.each do |user_id|
-              watcher = Watcher.new(:user_id => user_id, :watchable_type => "Issue", :watchable_id => issue.id)
-              watcher.save
+              user = User.find(user_id)
+              unless issue.watcher_user_ids.include? user_id
+                watcher = Watcher.new(:user_id => user_id, :watchable_type => "Issue", :watchable_id => issue.id)
+                watcher.save
+                if Redmine::Plugin.installed? :redmine_advanced_issue_history
+                  notes.append("Watcher #{user.name} was added")
+                end
+              end
             end
           end
         end
+      end
+      if notes.any? and Redmine::Plugin.installed? :redmine_advanced_issue_history
+        add_system_journal(notes, issue)
       end
     end
   end
